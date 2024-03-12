9a0b5d6449c555db830b507933ea2d7c6f261523
0xAckerMan <joelkores1@gmail.com>
2024-03-12 16:24:52 +0300

    Entering the realm of interfaces

--- src/0-index.ts
+++ src/0-index.ts
@@ -1,5 +1,5 @@
-let Name = "Kores"
-Name = "Joel"
+let Name = "Harry"
+Name = "Potter"
 console.log(Name)
 
 // functions in typescript
@@ -11,6 +11,7 @@ console.log(add(2, 10))
 console.log(add(add(2,3),4))
 
 //Collections(objects, arrays, tuples)
+//objects
 let person: {
     name: string
     email: string
@@ -54,5 +55,162 @@ printCar(car1)
 printCar(car2)
 
 
-console.log("Testinggg")
+//index signatures -> dictionaries
+
+const phones: {
+    [k: string]: {
+        country: string
+        area: string
+        number: string
+    } | undefined
+} = {} 
+
+// ARRAYS AND TUPLES
+// arrays
+const fileExtensions: string[] = ["js", "ts"]
+
+
+//tuples
+let student: [number, string, string] = [
+    9889,
+    "james",
+    "Karanja",
+]
+
+//destructuring a tupple
+const [adm, fname, lname] = student
+
+console.log(lname)
+
+
+//structural vs nominal types 
+//structural - printcar can work with either as long as it has the properties
+
+class Car {
+    make: string
+    model: string
+    year: number
+    isElectric: boolean
+}
+
+class Truck {
+    make: string
+    model: string
+    year: number
+    towingCapacity: number
+}
+
+const Vehicle = {
+    make: "Honda",
+    model: "Accord",
+    year: 2027,
+}
+
+printCar(Vehicle)
+printCar(new Car)
+printCar(new Truck)
+
+
+//union types |
+function flipCoin(): "heads" | "tails"{
+    if (Math.random() > 0.5) return "heads"
+    return "tails"
+}
+
+console.log(flipCoin())
+
+function maybeGetUserInfo(): ["error", Error] | ["success", {name: string; email: string}]{
+    if (flipCoin() === "heads") {
+        return[
+            "success",
+            {name: "John Doe", email: "john@mail.com"},
+        ]
+    } else {
+        return [
+            "error",
+            new Error("The coin landed on Tails :("),
+        ]
+    }
+}
+
+const outcome = maybeGetUserInfo()
+
+if (outcome[0] === "success") {
+    const userInfo = outcome[1];
+    console.log("User info retrieved successfully:");
+    console.log("Name:", userInfo.name);
+    console.log("Email:", userInfo.email);
+} else {
+    const error = outcome[1];
+    console.error("Error occurred:", error.message);
+}
+
+//intersection types &
+
+const ONE_WEEK = 7 * 24 * 60 * 60 * 1000;
+
+function makeWeek(): Date & {end: Date}{
+    const start = new Date()
+    const end = new Date(start.valueOf() + ONE_WEEK)
+
+    return {...start, end}
+}
+
+const thisWeek = makeWeek()
+//thisWeek.toISOString()
+console.log(thisWeek.end.toISOString())
+
+
+
+//interfaces and type aliases
+//type alias
+type UserContactInfo = {
+    name: string
+    email: string
+}
+
+function printContactInfo(info: UserContactInfo){
+    console.log(info)
+    console.log(info.email)
+}
+
+const person1 = {
+    "name": "Mike",
+    "email": "Mike@local.com"
+}
+
+printContactInfo(person1)
+
+//using type alias to clean the flipgetuser function
+
+type UserInfoOutcomeError = ["error", Error];
+type UserInfoOutcomeSuccess = ["success", { name: string; email: string }];
+type UserInfoOutcome = UserInfoOutcomeError | UserInfoOutcomeSuccess;
+
+function CleanedMaybeGetUserInfo(): UserInfoOutcome {
+    if (flipCoin() === "heads") {
+        return [
+            "success",
+            { name: "james", email: "smith@example.com" }
+        ];
+    } else {
+        return [
+            "error",
+            new Error("The coin landed on fail")
+        ];
+    }
+}
+const result = CleanedMaybeGetUserInfo();
+if (result[0] === "success") {
+    const userInfo = result[1];
+    console.log("User info retrieved successfully:");
+    console.log("Name:", userInfo.name);
+    console.log("Email:", userInfo.email);
+} else {
+    const error = result[1];
+    console.error("Error occurred while retrieving user info:", error.message);
+}
+
+
+// interfaces - Defines objects type
 
