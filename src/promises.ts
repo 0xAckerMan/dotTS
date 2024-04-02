0b5cea4d53eb8ec78c84fa001e627e8197458a04
0xAckerMan <joelkores1@gmail.com>
2024-04-02 11:05:41 +0300

    Return a promise

--- src/promises.ts
+++ src/promises.ts
@@ -1,3 +1,19 @@
+function uploadPhoto() {
+  return Promise.resolve({
+    status: 200,
+    body: 'photo-profile-1',
+  });
+}
+
+
+function createUser() {
+  return Promise.resolve({
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+  });
+}
+
+
 // Keep promises you can make and make promises you can Keep
 
 {
@@ -38,12 +54,73 @@
         })
     }
 
+
     getFullResponseFromAPI(true).then(
         (resp)=>{
             console.log(resp)
         }
     ).catch((error)=>{console.log("err: ", error)})
 
+    console.log("returning resolved resp\n\n")
+
 }
 
-// catch me if you can
+{
+    // catch me if you can
+    async function handleResponseFromAPI(promise: Promise<any>): Promise<{"status": number; "body": string}>{
+        return promise
+        .then(response =>{
+                console.log("Got a response the API");
+                return {status: 200, body: "success"}
+            })
+        .catch(error =>{
+                console.error("Error: ", error)
+                return {status: 0, body: ""}
+            })
+    }
+
+
+    (async ()=>{
+        const promise =Promise.resolve();
+        handleResponseFromAPI(promise);
+        console.log("Promise.resolve arg\n\n")
+    })()
+
+}
+
+
+{
+    async function handleProfileSignUp(): Promise<void>{
+        try{
+            const photoResult = await uploadPhoto()
+            const userResult = await createUser()
+
+            console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`)
+        }catch(error) {
+            console.error("Signup system offline")
+        }
+        
+    }
+
+    (async()=>{
+        await handleProfileSignUp()
+    })()
+    console.log("Testing\n\n")
+}
+
+{
+    function signUpUser(FirstName: string, LastName: string): Promise<{"FirstName": string; "LastName": string}>{
+        return Promise.resolve({
+            FirstName: FirstName,
+            LastName: LastName,
+        })
+    }
+
+    (async()=>{
+    console.log(signUpUser("Bob", "Dylan"));
+    console.log("Signup promise")
+    })()
+}
+
+
+
